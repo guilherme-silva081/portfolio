43d903203a4e397951786865563d560d6523b024
Guilherme Silva <guilhermemorais26092005@gmail.com>
2025-12-14 16:57:20 -0300

    subindo projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+  function enviarwhats (event){
+        event.preventDefault()
+        const nome=document.getElementById("nome").value;
+        const mensagem=document.getElementById("mensagem").value;
+        const telefone="5581987023658"
+
+        const texto=`Olá! me chamo ${nome}, ${mensagem}`
+        const msgFormatada=encodeURIComponent(texto)
+
+        const url=`https://wa.me/${telefone}?text=${msgFormatada}`
+        console.log(url)
+        window.open(url,"_blank")
+    }
